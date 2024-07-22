f4fda693ed07394db934858d7c92d28ecdd706d8
Sushil <147249755+Sushilgavit@users.noreply.github.com>
2024-07-22 20:13:10 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import HomePage from './components/HomePage'
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+       <Header /> 
+      <HomePage />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+
